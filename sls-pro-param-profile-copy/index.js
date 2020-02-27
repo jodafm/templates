73bffa30702264616e292c04e1cc9a4ae7604bd0
lukevandekieft <vandekie@gmail.com>
2020-02-27 15:56:25 -0800

    Create join array function using param results.

--- sls-pro-param-profile-copy/index.js
+++ sls-pro-param-profile-copy/index.js
@@ -2,20 +2,41 @@ require('dotenv').config();
 const axios = require('axios').default;
 
 module.exports = {
-  getParams: async function () {
+  joinArrays: async function (fromArray, toArray, sorter) {
+    let finalArray = toArray;
+    fromArray.forEach( forItem => {
+      let isDuplucate = false;
+      for (var i = 0; i < toArray.length; ++i) {
+        if (toArray[i][sorter] === forItem[sorter]) {
+          isDuplucate = true;
+          break;
+        }
+      }
+      if (!isDuplucate) {
+        finalArray.push(forItem);
+      }
+    })
+    return finalArray;
+  },
+  getParams: async function (profile) {
     console.log("Getting params:")
     const result = await axios.get(
-      `https://api.serverless.com/core/tenants/${process.env.SLS_ORG}/deploymentProfiles/${process.env.FROM_PROFILE}`,
+      `https://api.serverless.com/core/tenants/${process.env.SLS_ORG}/deploymentProfiles/${profile}`,
       { headers: { Authorization: `bearer ${process.env.TOKEN}` }}
     );
-    return result;
+    return result.data;
   },
   copyParams: async function () {
     console.log("Started copyParams function:")
-    const test = await this.getParams();
-    console.log("DONE");
-    console.log(test.data);
+    const fromParamsObj = await this.getParams(process.env.FROM_PROFILE);
+    const toParamsObj = await this.getParams(process.env.TO_PROFILE);
+    const paramList = await this.joinArrays(fromParamsObj.secrets, toParamsObj.secrets, "secretName");
+    const safeguardList = await this.joinArrays(fromParamsObj.safeguardsPolicies, toParamsObj.safeguardsPolicies, "title");
+    console.log(paramList);
+    console.log(safeguardList);
   }
 };
 
 const consoleCommand = {helloWorld: "node -e 'require(\"./index\").helloWorld()'"}
+
+// secretName
