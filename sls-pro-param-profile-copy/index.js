0361861bb86cc1cd049834f9852e10cb219b0bfc
lukevandekieft <vandekie@gmail.com>
2020-02-27 16:38:46 -0800

    Complete patch function and overall template functionality.

--- sls-pro-param-profile-copy/index.js
+++ sls-pro-param-profile-copy/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const axios = require('axios').default;
 
 module.exports = {
-  joinArrays: async function (fromArray, toArray, sorter) {
+  joinArrays: function (fromArray, toArray, sorter) {
     let finalArray = toArray;
     fromArray.forEach( forItem => {
       let isDuplucate = false;
@@ -18,25 +18,39 @@ module.exports = {
     })
     return finalArray;
   },
+
   getParams: async function (profile) {
-    console.log("Getting params:")
     const result = await axios.get(
       `https://api.serverless.com/core/tenants/${process.env.SLS_ORG}/deploymentProfiles/${profile}`,
       { headers: { Authorization: `bearer ${process.env.TOKEN}` }}
     );
     return result.data;
   },
+
+  patchParams: async function (requestBody) {
+    const result = await axios.patch(
+      `https://api.serverless.com/core/tenants/${process.env.SLS_ORG}/deploymentProfiles/${process.env.TO_PROFILE}`, 
+      requestBody,
+      { headers: { Authorization: `bearer ${process.env.TOKEN}` }}
+    );
+    return result;
+  },
+
   copyParams: async function () {
-    console.log("Started copyParams function:")
+    // get params for 'From' profile and 'To' profile
     const fromParamsObj = await this.getParams(process.env.FROM_PROFILE);
     const toParamsObj = await this.getParams(process.env.TO_PROFILE);
-    const paramList = await this.joinArrays(fromParamsObj.secrets, toParamsObj.secrets, "secretName");
-    const safeguardList = await this.joinArrays(fromParamsObj.safeguardsPolicies, toParamsObj.safeguardsPolicies, "title");
-    console.log(paramList);
-    console.log(safeguardList);
+    // join params and safeguards while deleting incoming 'From' values with duplicate names
+    const paramList = this.joinArrays(fromParamsObj.secrets, toParamsObj.secrets, "secretName");
+    const safeguardList = this.joinArrays(fromParamsObj.safeguardsPolicies, toParamsObj.safeguardsPolicies, "title");
+    // PATCH new results to 'To' profile
+    let requestObj = toParamsObj;
+    requestObj.secrets = paramList;
+    requestObj.safeguardsPolicies = safeguardList;
+    const result = await this.patchParams(requestObj);
+    console.log(result.data);
+    return result.data;
   }
 };
 
 const consoleCommand = {helloWorld: "node -e 'require(\"./index\").helloWorld()'"}
-
-// secretName
