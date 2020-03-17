6e8742b06203fafa693ad66bdc70fa543c839b13
Gary Jennings <garysjennings@gmail.com>
2020-03-17 14:45:00 -0600

    added eslint pattern

---
+++ nodejs-linting/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        fetch: false,
+        process: true
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        'no-var': 'warn',
+        'no-console': 'warn',
+        'max-nested-callbacks': ["warn", { "max": 2 }],
+        // curly: 'warn',
+        // 'no-caller': 'warn',
+        // 'no-eval': 'warn',
+        // 'no-throw-literal': 'warn',
+        // 'no-trailing-spaces': 'warn',
+        // 'no-undef-init': 'warn',
+        // 'no-unused-expressions': 'warn',
+        // 'prefer-const': 'warn',
+        // 'no-extra-semi': 'warn',
+        // 'no-new-wrappers': 'warn',
+        // eqeqeq: ['warn', 'smart'],
+        // 'keyword-spacing': 'warn',
+        // 'comma-spacing': 'warn',
+        // 'semi-spacing': 'warn',
+        // 'no-unused-vars': 'warn',
+        // 'no-prototype-builtins': 'warn',
+        // 'no-undef': 'warn',
+        // 'no-self-assign': 'warn',
+        // 'no-redeclare': 'warn'
+    }
+}
---
+++ nodejs-linting/README.md
@@ -0,0 +1,7 @@
+# Eslint Pattern
+Available EsLint Rules:
+https://eslint.org/docs/rules/
+
+
+### Walkthrough Video:
+https://www.loom.com/share/d9b69f2eceaa4774a5024cfc4f08d539
