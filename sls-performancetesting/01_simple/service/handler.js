ad428091428f152edc28e7d3b326d434117d1ad9
Gary Jennings <garysjennings@gmail.com>
2020-04-16 22:27:07 -0600

    added to sql template

--- sls-performancetesting/01_simple/service/handler.js
+++ sls-performancetesting/01_simple/service/handler.js
@@ -1,9 +1,59 @@
-module.exports.hello = async event => {
+const db = require('./db')
+
+module.exports.create = async event => {
+  const data = JSON.parse(event.body)
+  if (!data.id || !data.name) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: 'invalid'
+        }
+      )
+    }
+  }
+
+  const result = await db.create(data)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        data: 200
+        data: result
+      }
+    )
+  }
+}
+
+module.exports.get = async event => {
+  if (!event.queryStringParameters || !event.queryStringParameters.id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: 'invalid'
+        }
+      )
+    }
+  }
+  const id = event.queryStringParameters.id
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: 'invalid'
+        }
+      )
+    }
+  }
+
+  const result = await db.get(id)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        data: result
       }
     )
   }
