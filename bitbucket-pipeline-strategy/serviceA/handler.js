a4041dd6ec71ff3b67cc6f29ebe1730418e1488c
Gary Jennings <garysjennings@gmail.com>
2020-06-17 12:35:11 -0600

    updated packaging

--- bitbucket-pipeline-strategy/serviceA/handler.js
+++ bitbucket-pipeline-strategy/serviceA/handler.js
@@ -1,5 +1,12 @@
 const moment = require('moment')
+  
+const db = require('./db')
+
 module.exports.hello = async event => {
+  await db.create({
+    id: '123',
+    name: 'name'
+  })
   return {
     statusCode: 200,
     body: JSON.stringify({
