6902ec3dbcf77cf216e655509e33c1b15cee5e89
Gary Jennings <garysjennings@gmail.com>
2020-06-18 21:50:17 -0600

    added bitbucket piepline strat pattern

--- bitbucket-pipeline-strategy/README.md
+++ bitbucket-pipeline-strategy/README.md
@@ -1,2 +1,2 @@
 # Bitbucket Pipeline Strategy
-- [Video Walkthrough](https://www.loom.com/share/00f38867dbc74d1b9a070b572115ef1c)
+- [Walkthrough Video](https://www.loom.com/share/df85055c943b4e9b8119b67e4cb3b4af)
--- bitbucket-pipeline-strategy/serviceA/handler.js
+++ bitbucket-pipeline-strategy/serviceA/handler.js
@@ -10,7 +10,7 @@ module.exports.hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-        date: moment() + '_A_' + process.env.STAGE
+        date: moment() + '_A__' + process.env.STAGE
     })
   }
 }
